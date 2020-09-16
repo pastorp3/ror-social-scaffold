ab8a9bdb146ac503d9338d80f879f82792e6b17e
mateomh <mmojicah.84@gmail.com>
2020-09-16 12:04:45 -0500

    Add tests for the friendship model

--- spec/models/friendship_spec.rb
+++ spec/models/friendship_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Friendship, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  test_u = User.create(email: 'test2@test.com', name: 'mateo', gravatar_url: 'http://google.com', password: 'this is a password')
+  test_u1 = User.first
+  test_u = User.create(email: 'test@test.com', name: 'pastor', gravatar_url: 'http://google.com', password: 'this is a password')
+  test_u2 = User.last
+
+  it 'FAIL CHECK - Checks if a friendship can be created' do
+    test_f = Friendship.create(user_id: nil, friend_id: nil)
+    expect(test_f).to be_invalid
+  end
+
+  it 'SUCCESS CHECK - Checks if a friendship can be created' do
+    test_f = Friendship.create(user_id: test_u1.id , friend_id: test_u2.id, status: false)
+    expect(test_f).to be_valid
+  end
 end
