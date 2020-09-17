795b890f6c1e9e412e7f99facddb669b9414ac05
mateomh <mmojicah.84@gmail.com>
2020-09-17 16:51:19 -0500

    Add button to the individual user page for friend request and accept friend request

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -2,12 +2,12 @@
 module UserHelper
   def send_request(user)
     if current_user.check_friend_request(user.id) == false && current_user.id != user.id
-      link_to 'add_friend', "/friendship/#{user.id}"
+      link_to 'Add as a friend', "/friendship/#{user.id}"
     end
   end
 
   def accept_request(user)
-    link_to 'accept', "/friendship/#{user.id}", method: 'post' if current_user.check_accept_request(user)
+    link_to 'Accept Friend Request', "/friendship/#{user.id}", method: 'post' if current_user.check_accept_request(user)
   end
 end
 # rubocop: enable Style/GuardClause
